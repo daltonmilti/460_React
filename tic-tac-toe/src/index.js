601a3a0e9e2213ee45c52ac4d8523bdf3dfb2c9a
daltonmilti <147172231+daltonmilti@users.noreply.github.com>
2024-11-12 22:17:52 -0800

    done

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import Game from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Game />);
